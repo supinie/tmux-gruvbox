ca691c032fa02b264565ed10d028f0ded2e3725e
supinie <86788874+supinie@users.noreply.github.com>
2024-02-29 13:24:28 +0000

    source file first

--- get_git_repo.sh
+++ get_git_repo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd "$1" || exit
-git_repo=$(git rev-parse --show-toplevel 2>/dev/null)
+get_repo() {
+    cd "$1" || exit
+    git_repo=$(git rev-parse --show-toplevel 2>/dev/null)
 
-if [ -n "$git_repo" ]; then
-    echo "$(basename "$git_repo")"
-else
-    echo "#W"
-fi
+    if [ -n "$git_repo" ]; then
+        echo "$(basename "$git_repo")"
+    else
+        echo "#W"
+    fi
+}
